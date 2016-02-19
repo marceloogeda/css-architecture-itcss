e1e780444c9580d99439b7f7be79e0b5892aae13
Marcelo Ogeda <marcelo.ogeda@catho.com>
2016-02-19 18:27:21 -0200

    Add example project

---
+++ gulpfile.js
@@ -0,0 +1,20 @@
+'use strict';
+
+var gulp = require('gulp'),
+    sass = require('gulp-sass');
+
+gulp.task(
+    'sass',
+    function() {
+        return gulp.src('./sass/**/*.scss')
+            .pipe(sass().on('error', sass.logError))
+            .pipe(gulp.dest('./css'));
+    }
+);
+
+gulp.task(
+    'sass:watch',
+    function() {
+        gulp.watch('./sass/**/*.scss', ['sass']);
+    }
+);
